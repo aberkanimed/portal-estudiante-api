01452db1a381862015efae6a5546ce86ec75f3db
Mohamed Aberkani <aberkanimed@gmail.com>
2021-09-09 11:33:46 +0200

    La funcionalidad de Login terminada

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,10 +4,34 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+    public function login(Request $request) {
+        // Validar los datos
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        // Verificar que el email existe y que la contraseña es correcta
+        if (Auth::attempt($credentials)) {
+            $usuarioLogeado = Auth::user();
+            $token =  $usuarioLogeado->createToken('TokenUsuario')->plainTextToken;
+            $respuesta = [
+                'data' => [
+                    'usuario' => $usuarioLogeado,
+                    'token' => $token
+                ],
+            ];
+            return response()->json($respuesta);
+        } else {
+            return response()->json(['error'=>'Unauthorised'], 401);
+        }
+    }
+
     public function register(Request $request) {
         // Validar los datos
         $credentials = $request->validate([
